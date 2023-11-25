39801a86e7f50398a0a3d9af9839d6222c21c77b
José Daniel <josedanielgz@ufps.edu.co>
2023-11-24 20:00:43 -0500

    Soporte servicio de consulta de conferencias

--- src/main/java/com/ufps/conferencia/controller/ConferenciaController.java
+++ src/main/java/com/ufps/conferencia/controller/ConferenciaController.java
@@ -8,6 +8,9 @@ package com.ufps.conferencia.controller;
 
 import com.ufps.conferencia.entity.Conferencia;
 import com.ufps.conferencia.repository.ConferenciaRepository;
+import com.ufps.conferencia.service.ConferenciaService;
+
+import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ConferenciaController {
 
     @Autowired
-    ConferenciaRepository conferenciaRepository;
+    ConferenciaService conferenciaRepository;
     
          @GetMapping("/all")
     public List<Conferencia> getChairAll() {
 
-        return conferenciaRepository.findAll();
+        return conferenciaRepository.listaDeConferencias();
     }
+         
+         @GetMapping()
+         public List<Conferencia> conferenciasQueLesFaltanSieteDias(){
+        	 LocalDate fechaActual = LocalDate.now();
+        	 LocalDate fechaDentroDeSieteDias = fechaActual.plusDays(7);
+        	 return this.conferenciaRepository.conferenciasDentroDePlazoDeEntrega(fechaActual, fechaDentroDeSieteDias);
+         }
 }
---
+++ src/main/java/com/ufps/conferencia/service/impl/ConferenciaServiceImpl.java
@@ -0,0 +1,53 @@
+package com.ufps.conferencia.service.impl;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ufps.conferencia.entity.Conferencia;
+import com.ufps.conferencia.repository.ConferenciaRepository;
+import com.ufps.conferencia.service.ConferenciaService;
+
+public class ConferenciaServiceImpl implements ConferenciaService {
+	
+	@Autowired
+	ConferenciaRepository repositorio;
+
+	@Override
+	public Conferencia guardar(Conferencia unaConferencia) {
+		return this.repositorio.save(unaConferencia);
+	}
+
+	@Override
+	public Optional<Conferencia> buscar(Long idConferencia) {
+		return this.repositorio.findById(idConferencia);
+	}
+
+	@Override
+	public void eliminar(Long idConferencia) {
+		this.repositorio.deleteById(idConferencia);
+		
+	}
+
+	@Override
+	public Long diasDeRegistro(Conferencia unConferencia) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Conferencia> listaDeConferencias() {
+		// TODO Auto-generated method stub
+		return this.repositorio.findAll();
+	}
+
+	@Override
+	public List<Conferencia> conferenciasDentroDePlazoDeEntrega(LocalDate fechaInicial, LocalDate fechaFinal) {
+		return this.repositorio.findByPlazoEnvioTrabajosBetween(fechaInicial, fechaFinal);
+	}
+
+
+}
